ba755fe503146dfb27d76db20a86278fb5819d47
Josebuendia <ftmoloney@hotmail.com>
2020-03-30 00:10:02 +0100

    initial commit

---
+++ sswtjangolf/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :items
+ root 'static_pages#home'
+ 
+ get '/about' =>'static_pages#about'
+ get '/help' => 'static_pages#help'
+ 
+ 
+ 
+ # get 'static_pages/home'
+ # get 'static_pages/help'
+ # get 'static_pages/about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
