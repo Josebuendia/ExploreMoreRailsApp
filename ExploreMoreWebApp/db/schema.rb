52b0c19bee9c38f0615b0c3a928b45021a782f62
Josebuendia <ubuntu@ip-172-31-38-139.ec2.internal>
2020-04-15 13:58:18 +0000

    Created an orders scaffold for proccessing order.

--- ExploreMoreWebApp/db/schema.rb
+++ ExploreMoreWebApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_192945) do
+ActiveRecord::Schema.define(version: 2020_04_15_135655) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_04_07_192945) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "order_date"
+    t.integer "user_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
